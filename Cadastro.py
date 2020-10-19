5618e924086c7858850c3fe8079b0e0d5612824e
Luiz Felipe Souza <feh_jsouza@hotmail.com>
2020-10-19 13:18:15 -0300

    Create Cadastro.py
    Criação de um sistema de cadastro de nome e Hobby

---
+++ Cadastro.py
@@ -0,0 +1,8 @@
+# -*- coding = utf-8 -*-
+
+print('Olá, seja bem vindo ao sistema de cadastro e hobby!')
+
+nome = str(input('Informe seu nome completo: '))
+Hobby = str(input('E agora informe o seu hobby: '))
+
+print('Muito obrigado!')
